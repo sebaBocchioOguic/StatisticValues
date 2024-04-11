a0d00d19516146fe6491d81085c1244d9ce9007d
Sebastian Bocchio Oguic <60882728+sebaBocchioOguic@users.noreply.github.com>
2024-04-11 13:11:18 +0200

    Initial Upload
    Initial Uploaded of the scratches of the project

---
+++ FormMain.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ProyectoEstadistico
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOriginalValues = new System.Windows.Forms.TextBox();
+            this.lblOriginalValues = new System.Windows.Forms.Label();
+            this.lblOriginalValuesDesc = new System.Windows.Forms.Label();
+            this.lblAddNumber = new System.Windows.Forms.Label();
+            this.txtAddNumber = new System.Windows.Forms.TextBox();
+            this.btnAddNumber = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOriginalValues
+            // 
+            this.txtOriginalValues.Location = new System.Drawing.Point(78, 111);
+            this.txtOriginalValues.Multiline = true;
+            this.txtOriginalValues.Name = "txtOriginalValues";
+            this.txtOriginalValues.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOriginalValues.Size = new System.Drawing.Size(928, 168);
+            this.txtOriginalValues.TabIndex = 0;
+            // 
+            // lblOriginalValues
+            // 
+            this.lblOriginalValues.AutoSize = true;
+            this.lblOriginalValues.Location = new System.Drawing.Point(75, 37);
+            this.lblOriginalValues.Name = "lblOriginalValues";
+            this.lblOriginalValues.Size = new System.Drawing.Size(98, 16);
+            this.lblOriginalValues.TabIndex = 1;
+            this.lblOriginalValues.Text = "Original Values";
+            this.lblOriginalValues.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblOriginalValuesDesc
+            // 
+            this.lblOriginalValuesDesc.AutoSize = true;
+            this.lblOriginalValuesDesc.Location = new System.Drawing.Point(180, 37);
+            this.lblOriginalValuesDesc.Name = "lblOriginalValuesDesc";
+            this.lblOriginalValuesDesc.Size = new System.Drawing.Size(504, 16);
+            this.lblOriginalValuesDesc.TabIndex = 2;
+            this.lblOriginalValuesDesc.Text = "Here you can write every number from which you need to obtain final statistics va" +
+    "lues";
+            // 
+            // lblAddNumber
+            // 
+            this.lblAddNumber.AutoSize = true;
+            this.lblAddNumber.Location = new System.Drawing.Point(78, 68);
+            this.lblAddNumber.Name = "lblAddNumber";
+            this.lblAddNumber.Size = new System.Drawing.Size(83, 16);
+            this.lblAddNumber.TabIndex = 3;
+            this.lblAddNumber.Text = "Add Number";
+            // 
+            // txtAddNumber
+            // 
+            this.txtAddNumber.Location = new System.Drawing.Point(206, 68);
+            this.txtAddNumber.Name = "txtAddNumber";
+            this.txtAddNumber.Size = new System.Drawing.Size(100, 22);
+            this.txtAddNumber.TabIndex = 4;
+            this.txtAddNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnAddNumber
+            // 
+            this.btnAddNumber.Location = new System.Drawing.Point(330, 66);
+            this.btnAddNumber.Name = "btnAddNumber";
+            this.btnAddNumber.Size = new System.Drawing.Size(75, 23);
+            this.btnAddNumber.TabIndex = 5;
+            this.btnAddNumber.Text = "Add";
+            this.btnAddNumber.UseVisualStyleBackColor = true;
+            this.btnAddNumber.Click += new System.EventHandler(this.btnAddNumber_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 637);
+            this.Controls.Add(this.btnAddNumber);
+            this.Controls.Add(this.txtAddNumber);
+            this.Controls.Add(this.lblAddNumber);
+            this.Controls.Add(this.lblOriginalValuesDesc);
+            this.Controls.Add(this.lblOriginalValues);
+            this.Controls.Add(this.txtOriginalValues);
+            this.Name = "FormMain";
+            this.Text = "FormMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOriginalValues;
+        private System.Windows.Forms.Label lblOriginalValues;
+        private System.Windows.Forms.Label lblOriginalValuesDesc;
+        private System.Windows.Forms.Label lblAddNumber;
+        private System.Windows.Forms.TextBox txtAddNumber;
+        private System.Windows.Forms.Button btnAddNumber;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoEstadistico
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddNumber_Click(object sender, EventArgs e)
+        {
+            /* This function adds the new number in the TextField to the main group of numbers */
+
+            // It adds a separator only if it is not the first number
+            if(txtOriginalValues.Text != "")
+            {
+                txtOriginalValues.Text += " - ";
+            }
+            
+            // Adds the number to the main set
+            txtOriginalValues.Text += txtAddNumber.Text;
+
+            // Clear the input TextField of new number
+            txtAddNumber.Text = "";
+        }
+    }
+}
