38ee5734ec06bdc1ef53519f00794431aa7088d3
Sebastian Bocchio Oguic <sbocchio@gmail.com>
2024-04-14 21:27:45 +0200

    New menu structure in Console Version
    Add Menu in Console Version
    Add Try/Catch segment

--- StatisticValuesConsole/Program.cs
+++ StatisticValuesConsole/Program.cs
@@ -8,8 +8,59 @@ namespace StatisticValuesConsoleWin
 {
     internal class Program
     {
+        /// <summary>
+        /// Original Console Code for the core of the solution
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
+            // Initialize Byte Variable for Menu Index
+            byte menuOption = 1;
+
+            while(menuOption == 1 || menuOption == 2)
+            {
+                // Menu Description in Console
+                Console.WriteLine("MENU");
+                Console.WriteLine("1 - Add a new number");
+                Console.WriteLine("2 - View the complete numbers set");
+                Console.WriteLine("3 - Exit");
+                Console.Write("---> ");
+
+
+                try
+                {
+                    
+                    // Input Menu Option
+                    menuOption = byte.Parse(Console.ReadLine());
+
+                    switch (menuOption)
+                    {
+                        case 1:
+                        case 2:
+                            continue;
+                        case 3:
+                            break;
+                        
+                        // Invalid options generates an Exception
+                        default:
+                            throw new Exception("Invalid Option");
+                    }
+
+                }
+                catch (Exception e)
+                {
+
+                    // Shows the error message and finishes the program
+                    Console.WriteLine(e.Message + "\nPress any key to exit program");
+                    Console.ReadKey();
+                    break;
+
+                }
+
+                
+            }
+            
         }
     }
 }
