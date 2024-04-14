a241441dff63be6ce610ec5f7bd0429bd8f5d9dd
Sebastian Bocchio Oguic <sbocchio@gmail.com>
2024-04-14 23:21:46 +0200

    Modified addNumber() and added showNumbers()
    This deploy modifies addNumber() passing the complete list by reference to the function.
    Also creates showNumbers() with the complete list by reference and shows the numbers in the console.
    
    Maybe in the future is better to avoid passing the List by reference and just initialize the List as a Global List in Program.cs. But at this time it's only laboratory so it's useful and simple to use it this way

--- StatisticValuesConsole/Program.cs
+++ StatisticValuesConsole/Program.cs
@@ -8,6 +8,7 @@ namespace StatisticValuesConsoleWin
 {
     internal class Program
     {
+
         /// <summary>
         /// Original Console Code for the core of the solution
         /// </summary>
@@ -15,13 +16,16 @@ namespace StatisticValuesConsoleWin
         static void Main(string[] args)
         {
 
+            // Initialize List of Int Values
+            List<int> valuesList = new List<int>();
+
             // Initialize Byte Variable for Menu Index
             byte menuOption = 1;
 
             while(menuOption == 1 || menuOption == 2)
             {
                 // Menu Description in Console
-                Console.WriteLine("MENU");
+                Console.WriteLine("\nMENU");
                 Console.WriteLine("1 - Add a new number");
                 Console.WriteLine("2 - View the complete numbers set");
                 Console.WriteLine("3 - Exit");
@@ -37,13 +41,21 @@ namespace StatisticValuesConsoleWin
                     switch (menuOption)
                     {
                         case 1:
+                            // Add a new value to the List
+                            addNumber(ref valuesList);
+                            break;
+
                         case 2:
+                            // Show the content of the List
+                            showNumbers(ref valuesList);
                             continue;
+
                         case 3:
+                            // Exit the program
                             break;
                         
-                        // Invalid options generates an Exception
                         default:
+                            // Invalid options generates an Exception
                             throw new Exception("Invalid Option");
                     }
 
@@ -61,6 +73,32 @@ namespace StatisticValuesConsoleWin
                 
             }
             
+        }
+
+        /// <summary>
+        /// Add a new number to the List
+        /// </summary>
+        /// <param name="valuesList">Complete List of values by reference</param>
+        static void addNumber(ref List<int> valuesList)
+        {
+            Console.Write("\nInsert the new value: -> ");
+            int newValue = Convert.ToInt32(Console.ReadLine());
+            valuesList.Add(newValue);
+            Console.WriteLine($"New value {newValue} added.\n");
+        }
+
+        /// <summary>
+        /// Show the content of the List
+        /// </summary>
+        /// <param name="valuesList">Complete List of values by reference</param>
+        static void showNumbers(ref List<int> valuesList)
+        {
+            Console.Write("\nNumbers List: ");
+            foreach (int number in valuesList)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine("");
         }
     }
 }
